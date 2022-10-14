9ff99edbf7db75b832268bc867903cc14d231c7f
Enzo Innocenzi <enzo@innocenzi.dev>
2022-10-14 15:22:04 +0200

    feat: add sponsors

---
+++ sponsorkit.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+	// Providers configs
+	github: {
+		login: 'innocenzi',
+		type: 'user',
+	},
+
+	// Rendering configs
+	width: 800,
+	formats: ['json', 'svg', 'png'],
+	tiers: [
+		{
+			title: 'Backers',
+			preset: presets.base,
+		},
+		{
+			title: 'Sponsors',
+			monthlyDollars: 15,
+			preset: presets.medium,
+		},
+		{
+			title: 'Gold sponsors',
+			monthlyDollars: 50,
+			preset: presets.large,
+		},
+	],
+})
